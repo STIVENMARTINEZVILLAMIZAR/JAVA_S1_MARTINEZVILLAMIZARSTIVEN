9e208d7559737e5a340532e127c69788344b082d
SamuelMogollon <smogollonjanne@gmail.com>
2025-12-15 10:37:52 -0500

    FEAT: :zap: FIRST COMMIT

---
+++ src/java_s1_martinezvillamizarstiven/helloWorld.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package java_s1_martinezvillamizarstiven;
+
+/**
+ *
+ * @author camper
+ */
+public class helloWorld {
+  public static void main(String[] args) {
+    String name = "John";
+    System.out.println("Hello " + name);
+  }
+}
