f7dbbe5a9319982baf1e7372b84be4c4176f1139
SamuelMogollon <smogollonjanne@gmail.com>
2025-12-16 09:24:55 -0500

    FEAT: :zap: CLASS CICLOS AND PROJECT TIENDA

---
+++ src/TIENDA/TIENDA.java
@@ -0,0 +1,33 @@
+
+
+package TIENDA;
+
+/** 
+ * @author STIVEN MARTINEZ VILLAMIZAR
+ */
+
+import java.util.Scanner;
+
+
+public class TIENDA{
+    public static void main(String[] args) {
+        // Definimos credencia de usuario
+    string nombre_User = "zeven";
+    string  clave_user= "1416";
+    int opciones_menu= 0;
+    
+  
+
+
+       System.out.println("""
+                ****************************
+                        ¡BIENVENIDOS!
+                 1. EMPEZAR
+                *****************************
+                """);
+
+
+}
+
+
+}
